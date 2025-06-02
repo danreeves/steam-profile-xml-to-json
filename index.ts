b5e39b59adbe35e4a4d23055fb703b49ae2fb195
Dan Reeves <hey@danreev.es>
2025-06-02 20:29:24 +0100

    use fast-xml-parser

--- index.ts
+++ index.ts
@@ -1,19 +1,22 @@
-import { parse } from "https://deno.land/x/xml@2.1.3/mod.ts";
-const NOT_FOUND = JSON.stringify({ error: "not found"})
+import { XMLParser } from "fast-xml-parser";
+const NOT_FOUND = JSON.stringify({ error: "not found" });
+const parser = new XMLParser({
+  ignoreDeclaration: true,
+});
 
 Deno.serve(async (request: Request) => {
   const steamId = request.url.split("/").pop() || "";
 
   if (steamId) {
     const response = await fetch(
-      `https://steamcommunity.com/profiles/${steamId}?xml=1`
+      `https://steamcommunity.com/profiles/${steamId}?xml=1`,
     );
     if (response.ok) {
       const text = await response.text();
-      const json = parse(text);
+      const json = parser.parse(text);
 
       if (json.profile) {
-        console.log(response.status, request.url)
+        console.log(response.status, request.url);
         return new Response(JSON.stringify(json), {
           headers: { "content-type": "application/json" },
         });
@@ -21,11 +24,11 @@ Deno.serve(async (request: Request) => {
     }
   }
 
-  console.log(404, request.url)
-  return new Response(NOT_FOUND, { 
+  console.log(404, request.url);
+  return new Response(NOT_FOUND, {
     status: 404,
     headers: {
-      "content-type": "application/json" 
-    }
+      "content-type": "application/json",
+    },
   });
 });
