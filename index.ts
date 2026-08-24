e1187bd2aed60b41c40735acd64f31a5316177f0
Dan Reeves <hey@danreev.es>
2026-08-24 19:37:46 +0100

    migrate worker to Cloudflare runtime

--- index.ts
+++ index.ts
@@ -4,31 +4,33 @@ const parser = new XMLParser({
   ignoreDeclaration: true,
 });
 
-Deno.serve(async (request: Request) => {
-  const steamId = request.url.split("/").pop() || "";
+export default {
+  async fetch(request: Request): Promise<Response> {
+    const steamId = request.url.split("/").pop() || "";
 
-  if (steamId) {
-    const response = await fetch(
-      `https://steamcommunity.com/profiles/${steamId}?xml=1`,
-    );
-    if (response.ok) {
-      const text = await response.text();
-      const json = parser.parse(text);
+    if (steamId) {
+      const response = await fetch(
+        `https://steamcommunity.com/profiles/${steamId}?xml=1`,
+      );
+      if (response.ok) {
+        const text = await response.text();
+        const json = parser.parse(text);
 
-      if (json.profile) {
-        console.log(response.status, request.url);
-        return new Response(JSON.stringify(json), {
-          headers: { "content-type": "application/json" },
-        });
+        if (json.profile) {
+          console.log(response.status, request.url);
+          return new Response(JSON.stringify(json), {
+            headers: { "content-type": "application/json" },
+          });
+        }
       }
     }
-  }
 
-  console.log(404, request.url);
-  return new Response(NOT_FOUND, {
-    status: 404,
-    headers: {
-      "content-type": "application/json",
-    },
-  });
-});
+    console.log(404, request.url);
+    return new Response(NOT_FOUND, {
+      status: 404,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  },
+};
